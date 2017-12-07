50b58e7e3024765f31a203de09ce6eed0111a11c
sienrik15 <enrique_sam11@hotmail.com>
2017-12-07 15:44:37 -0500

    integrando nuevo modulo pro

---
+++ matrialuipro/src/main/java/com/joinnus/utilities/matrialuipro/NavegationPro.java
@@ -0,0 +1,24 @@
+package com.joinnus.utilities.matrialuipro;
+
+/**
+ * Created by User on 7/12/2017.
+ */
+
+public class NavegationPro {
+
+    String sam;
+    NavegationPro(){
+        sam = "hola mundo";
+    }
+
+
+    public String getSam() {
+        return sam;
+    }
+
+    public void setSam(String sam) {
+        this.sam = sam;
+    }
+
+
+}
